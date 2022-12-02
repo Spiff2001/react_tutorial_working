bea5754c33dac188fd3156dea3c39d2f41ba2b45
spiff2001 <willglick@icloud.com>
2022-12-02 09:05:34 -0500

    work

---
+++ react_api_project/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+
+
+function App() {
+  return(
+  <div className="App">
+    <h2>React JSON</h2>
+    <Home/>
+    </div>
+  )
+}
+
+export default App;
